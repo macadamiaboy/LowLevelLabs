45b5128ff79b376772c52525c3f193c71b1c739e
macadamiaboy <71150520+macadamiaboy@users.noreply.github.com>
2022-01-13 17:46:40 +0300

    Add files via upload

--- lab5/src/library.c
+++ lab5/src/library.c
@@ -18,8 +18,8 @@ graph createGraph(int size) {
 }
 
 void addVertex(graph currentGraph) {
-    int size = currentGraph->size + 1;
-    currentGraph->size = size;
+    currentGraph->size++;
+    int size = currentGraph->size;
     currentGraph->list = (vertex *) realloc(currentGraph->list, size * sizeof(vertex *));
     vertex newVertex = (vertex) malloc(sizeof(*newVertex));
     newVertex->number = size;
@@ -33,10 +33,8 @@ void connect(graph currentGraph, int first, int second) {
     vertex firstVertex = NULL;
     vertex secondVertex = NULL;
     for (int i = 0; i < size; i++) {
-        if (currentGraph->list[i] != NULL) {
-            if (currentGraph->list[i]->number == first) firstVertex = currentGraph->list[i];
-            else if (currentGraph->list[i]->number == second) secondVertex = currentGraph->list[i];
-        }
+        if (currentGraph->list[i]->number == first) firstVertex = currentGraph->list[i];
+        else if (currentGraph->list[i]->number == second) secondVertex = currentGraph->list[i];
     }
     if (firstVertex == NULL || secondVertex == NULL) {
         printf("You've chosen illegal arguments \n");
@@ -61,38 +59,45 @@ void connect(graph currentGraph, int first, int second) {
 void deleteVertex(graph currentGraph, int item) {
     if (item == 0) return;
     for (int i = 0; i < currentGraph->size; i++) {
-        if (currentGraph->list[i] != NULL) {
-            if (currentGraph->list[i]->number == item) {
-                currentGraph->list[i] = NULL;
-                continue;
+        if (currentGraph->list[i]->number == item) {
+            currentGraph->list[i] = NULL;
+            continue;
+        }
+        vertex currentVertex = currentGraph->list[i];
+        for (int j = 0; j < currentVertex->connections; j++) {
+            if (currentVertex->neighbours[j] == item) {
+                currentVertex->neighbours[j] = 0;
+                sortNilsInArray(currentVertex->neighbours, currentVertex->connections);
+                currentVertex->connections--;
+                currentVertex->neighbours = (int *)
+                        realloc(currentVertex->neighbours, currentVertex->connections * sizeof(int));
+                break;
             }
-            vertex currentVertex = currentGraph->list[i];
-            for (int j = 0; j < currentVertex->connections; j++) {
-                if (currentVertex->neighbours[j] == item) {
-                    currentVertex->neighbours[j] = 0;
-                    sortNilsInArray(currentVertex->neighbours, currentVertex->connections);
-                    currentVertex->connections--;
-                    currentVertex->neighbours = (int *)
-                            realloc(currentVertex->neighbours, currentVertex->connections * sizeof(int));
-                    break;
-                }
-            }
-        } else printf("This vertex is doesn't exist \n");
+        }
+    }
+    for (int i = 0; i < currentGraph->size; i++) {
+        if (currentGraph->list[i] == NULL) {
+            sortVertices(currentGraph, currentGraph->size);
+            currentGraph->size--;
+            currentGraph->list =
+                    (vertex *) realloc(currentGraph->list, currentGraph->size * sizeof(vertex *));
+            break;
+        }
     }
 }
+
+//void search(graph currentGraph, int knownVertex, int unknownVertex) { }
 
 //для проверки и отладки
 void checkGraph(graph currentGraph) {
     for (int i = 0; i < currentGraph->size; i++) {
-        if (currentGraph->list[i] != NULL) {
-            vertex currentVertex = currentGraph->list[i];
-            printf("Graph %d with %d connections \n", currentVertex->number, currentVertex->connections);
-            printf("Its neighbour(s) is(are) ");
-            for (int j = 0; j < currentVertex->connections; j++) {
-                printf("%d ", currentVertex->neighbours[j]);
-            }
-            printf("\n \n");
+        vertex currentVertex = currentGraph->list[i];
+        printf("Graph %d with %d connections \n", currentVertex->number, currentVertex->connections);
+        printf("Its neighbour(s) is(are) ");
+        for (int j = 0; j < currentVertex->connections; j++) {
+            printf("%d ", currentVertex->neighbours[j]);
         }
+        printf("\n \n");
     }
 }
 
@@ -111,6 +116,16 @@ void sortNilsInArray(int* array, int size) {
             int zero = array[i];
             array[i] = array[i + 1];
             array[i + 1] = zero;
+        }
+    }
+}
+
+void sortVertices(graph currentGraph, int size) {
+    for (int i = 0; i < (size - 1); i++) {
+        if (currentGraph->list[i] == NULL) {
+            vertex nullable = currentGraph->list[i];
+            currentGraph->list[i] = currentGraph->list[i + 1];
+            currentGraph->list[i + 1] = nullable;
         }
     }
 }
