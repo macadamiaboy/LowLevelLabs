ac5f57264a39017bdfc535a236243425cc2d19b8
macadamiaboy <71150520+macadamiaboy@users.noreply.github.com>
2022-01-14 21:10:36 +0300

    Add files via upload

--- lab5/hdrs/library.h
+++ lab5/hdrs/library.h
@@ -1,8 +1,10 @@
 #include <stdio.h>
+#include <stdbool.h>
 #ifndef CLIB_LIBRARY_H
 #define CLIB_LIBRARY_H
 
 typedef struct item {
+    bool seen;
     int number;
     int connections;
     int *neighbours;
@@ -20,6 +22,9 @@ void connect(graph currentGraph, int first, int second);
 void checkGraph(graph currentGraph);
 void clearMemory(graph currentGraph);
 void sortNilsInArray(int* array, int size);
+int* search(graph currentGraph, int knownVertex, int unknownVertex);
+void sortVertices(graph currentGraph, int size);
+int* recursiveSearch(vertex current, int steps, int* way, graph currentGraph, int unknownVertex);
 
 
 #endif //CLIB_LIBRARY_H
